c6ce2960e117bc1677e1e689dd5db7f8dc899fb0
Mitchell Solomon <msolomonqwerty@gmail.com>
2021-11-25 11:45:26 -0500

    fix theme mode

--- components/ThemeIcon.js
+++ components/ThemeIcon.js
@@ -1,21 +1,30 @@
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 export default function ThemeIcon() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "light") {
+      return <BsFillSunFill role="button" onClick={() => setTheme("dark")} />;
+    } else {
+      return <BsFillMoonFill role="button" onClick={() => setTheme("light")} />;
+    }
+  };
 
   return (
     <>
-      <button
-        aria-label="Toggle Theme Mode"
-        title="Toggle Theme Mode"
-        type="button"
-        onClick={() => {
-          setTheme(theme === "light" ? "dark" : "light");
-        }}
-      >
-        {theme === "light" ? <BsFillSunFill /> : <BsFillMoonFill />}
-      </button>
+      <button>{renderThemeChanger()}</button>
     </>
   );
 }
