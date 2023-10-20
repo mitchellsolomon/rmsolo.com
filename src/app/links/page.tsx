471a4652778e84193ff63fca19270ba296c7d8bd
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-10-19 23:25:57 -0400

    update links

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -2,18 +2,23 @@ export default function Links() {
   return (
     <div className="flex flex-col justify-start items-center">
       <div className="flex flex-col justify-center items-center gap-4">
-        <a href="https://github.com" className="text-xl font-bold">
+        <a
+          href="https://github.com/mitchellsolomon"
+          className="text-xl font-bold"
+          target="_blank"
+        >
           GitHub
         </a>
-        <a href="https://linkedin.com" className="text-xl font-bold">
+        <a
+          href="https://www.linkedin.com/in/reaganmitchellsolomon/"
+          className="text-xl font-bold"
+          target="_blank"
+        >
           LinkedIn
         </a>
-        <a href="https://resume.com" className="text-xl font-bold">
+        {/* <a href="https://resume.com" className="text-xl font-bold">
           Resume
-        </a>
-        <a href="mailto:me@example.com" className="text-xl font-bold">
-          Email
-        </a>
+        </a> */}
       </div>
     </div>
   );
