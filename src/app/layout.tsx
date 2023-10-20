9a2bcefc0de3ac3629c76e1f456ef27582c455b4
Mitchell Solomon <msolomonqwerty@gmail.com>
2023-10-19 23:50:21 -0400

    adjust padding again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-neutral-50 dark:bg-[#0e0e0f] text-black dark:text-white flex flex-col items-center max-w-sm sm:max-w-lg md:max-w-2xl mt-6 sm:mt-10 md:mt-14 min-h-screen w-full mx-auto px-6 md:px-0`}
+        className={`${inter.className} bg-neutral-50 dark:bg-[#0e0e0f] text-black dark:text-white flex flex-col items-center max-w-sm sm:max-w-lg md:max-w-2xl mt-6 sm:mt-10 md:mt-14 min-h-screen w-full mx-auto px-5 md:px-0 sm:px-0`}
       >
         <main className="w-full">
           <Navbar />
