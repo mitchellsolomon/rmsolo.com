d05c663e7d913e1c5b92668467e6b82f4effa186
Mitchell Solomon <msolomonqwerty@gmail.com>
2024-04-24 15:39:05 -0700

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from "./components/navbar";
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
